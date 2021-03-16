09f8a2fc0f27d4d6226fef41a79d20724f033bbb
danielgard1 <danielgardner1357@gmail.com>
2021-03-15 19:22:52 -0600

    Exercise Complete

---
+++ BasicClasses Exercise/CarClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClasses_Exercise
+{
+    class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+    }
+}
---
+++ BasicClasses Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicClasses_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myFirstCar = new Car();
+
+            myFirstCar.Make = "Toyota";
+            myFirstCar.Model = "Tacoma";
+            myFirstCar.Year = 1999;
+
+            Console.WriteLine($"My first car was a {myFirstCar.Make} {myFirstCar.Model} that was made in the year {myFirstCar.Year}");
+        }
+    }
+}
